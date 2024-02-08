03e866f508eb2636816a1a0a0232e5d66321f9ab
Asli08 <s_aydemir22@stud.hwr-berlin.de>
2024-02-08 16:30:59 +0100

    login.html und register.html mit app.py

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields import DateField  # Änderung des Imports für DateField
+from wtforms.validators import InputRequired
+import os
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+tasks = []
+documents = []
+notes = []
+events = []
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx', 'txt'}
+
+class EventForm(FlaskForm):
+    date = DateField('Date', validators=[InputRequired()])
+    event = StringField('Event', validators=[InputRequired()])
 
 @app.route('/')
 def index():
@@ -19,3 +40,65 @@ def register():
     if request.method == 'POST':
         return redirect(url_for('home'))
     return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/2DO', methods=['GET', 'POST'])
+def todo():
+    if request.method == 'POST':
+        task_name = request.form['task']
+        tasks.append({'name': task_name, 'completed': False})
+    return render_template('2DO.html', tasks=tasks)
+
+@app.route('/delete_task/<int:index>')
+def delete_task(index):
+    del tasks[index]
+    return redirect(url_for('todo'))
+
+@app.route('/2DATE', methods=['GET', 'POST'])
+def date():
+    form = EventForm()
+    if form.validate_on_submit():
+        event_date = form.date.data
+        event = form.event.data
+        events.append({'date': event_date, 'event': event})
+        return redirect(url_for('date'))
+    return render_template('2DATE.html', form=form, events=events)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/2DOC', methods=['GET', 'POST'])
+def doc():
+    if request.method == 'POST':
+        if 'document' in request.files:
+            file = request.files['document']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                documents.append({'filename': filename})
+                return redirect(url_for('doc'))
+        elif 'note' in request.form:
+            note = request.form['note']
+            notes.append(note)
+            return redirect(url_for('doc'))
+    return render_template('2DOC.html', documents=documents, notes=notes)
+
+@app.route('/view_document/<filename>')
+def view_document(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/profil')
+def profil():
+    return render_template('profil.html')
+
+@app.route('/back')
+def back():
+    referrer = request.referrer or url_for('home')
+    return redirect(referrer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
